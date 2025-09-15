8539d25fd986be5d30ffc9289f69ebf911fc070e
DJanneyNCC <david.janney@student.northampton.edu>
2025-09-14 22:54:39 -0400

    Update permutations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -50,5 +50,11 @@ void permutations2 (int array[], size_t size) {
 }
 
 void permutations3 (int array[], size_t size) {
-    // TODO: Implement algorithm #3 here
+    for (int i = 0; i < size; i++) {
+        array[i] = (i + 1);
+    }
+    /*
+    for (i to n-1) {
+        
+    }*/
 }
